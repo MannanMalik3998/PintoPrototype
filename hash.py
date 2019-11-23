aaddc9ec4b51e087e8e352137b01ce3eb8147a48
MannanMalik3998 <manan.hameed47@gmail.com>
2019-11-23 20:17:46 +0500

    SignedImage

--- hash.py
+++ hash.py
@@ -24,3 +24,23 @@ def hash_file(filename):
 message = hash_file(imagePath)
 print(message)
 print(hash_file(imagePath)==hash_file(imagePath))
+
+import OpenSSL
+from OpenSSL import crypto
+import base64
+key_file = open("C:\my.pem", "r")
+key = key_file.read()
+key_file.close()
+password = "password of prk"
+
+if key.startswith('-----BEGIN '):
+    pkey = crypto.load_privatekey(crypto.FILETYPE_PEM, key, password)
+else:
+    pkey = crypto.load_pkcs12(key, password).get_privatekey()
+print pkey
+data = "data"
+sign = OpenSSL.crypto.sign(pkey, data, "sha256") 
+print sign
+
+data_base64 = base64.b64encode(sign)
+print data_base64
---
+++ pyopenssl_x509_signverify_example.py
@@ -0,0 +1,87 @@
+from OpenSSL import crypto
+from socket import gethostname
+import cv2
+
+CERT_FILE = "selfsigned.crt"
+KEY_FILE = "private.key"
+imagePath='images\\1.pngPixelated.png'
+imagePath2='images\\1.pngPixelated2.png'
+
+import hashlib
+def hash_file(filename):
+   """"This function returns the SHA-1 hash
+   of the file passed into it"""
+   # make a hash object
+   h = hashlib.sha1()
+   
+   # open file for reading in binary mode
+   with open(filename,'rb') as file:
+       # loop till the end of the file
+       chunk = 0
+       while chunk != b'':
+           # read only 1024 bytes at a time
+           chunk = file.read(1024)
+           h.update(chunk)
+   # return the hex representation of digest
+   return h.hexdigest()
+
+def makeCert():#generate a self signed certificate
+    k = crypto.PKey()
+    k.generate_key(crypto.TYPE_RSA, 2048)  # generate RSA key-pair
+
+
+    cert = crypto.X509()
+    cert.get_subject().C = "PK"
+    cert.get_subject().ST = "Karachi"
+    cert.get_subject().O = "FAST-NUCES"
+    cert.get_subject().OU = "FAST-NUCES"
+    cert.get_subject().CN = gethostname()
+    cert.set_serial_number(1000)
+    cert.gmtime_adj_notBefore(0)
+    cert.gmtime_adj_notAfter(10*365*24*60*60)  # 10 years expiry date
+    cert.set_issuer(cert.get_subject())  # self-sign this certificate
+
+    cert.set_pubkey(k)
+    cert.sign(k, 'sha256')
+
+   
+
+    # pass certificate around, but of course keep private.key
+
+    import os
+
+    open(CERT_FILE, 'a').close()
+    open(KEY_FILE, 'a').close()
+
+    f = os.open(CERT_FILE,os.O_WRONLY)
+    os.write(f, crypto.dump_certificate(crypto.FILETYPE_PEM, cert))
+    os.close(f)
+
+    f2=os.open(KEY_FILE,os.O_WRONLY)
+    os.write(f2, crypto.dump_privatekey(crypto.FILETYPE_PEM, k))
+    os.close(f2)
+
+
+# makeCert() # call to make certificate
+
+# Now the real world use case; use certificate to verify signature
+f = open(KEY_FILE)
+pv_buf = f.read()
+f.close()
+priv_key = crypto.load_privatekey(crypto.FILETYPE_PEM, pv_buf)
+
+#open certificate
+f = open(CERT_FILE)
+ss_buf = f.read()
+f.close()
+ss_cert = crypto.load_certificate(crypto.FILETYPE_PEM, ss_buf)
+
+try:
+    # # sign and verify PASS
+    msg=hash_file(imagePath)
+    sig = crypto.sign(priv_key, msg, 'sha256')
+    crypto.verify(ss_cert, sig, hash_file(imagePath2), 'sha256')
+    print("Successfully signed")
+
+except:
+    print("Signing failed due to corrupted data")
